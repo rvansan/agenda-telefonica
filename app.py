f0c553e6b5ded8c4c687fd2ece58870fba9bb97d
Rafael Vansan <rafaelvansan@MacBook-Air-de-Rafael.local>
2025-07-03 13:41:04 -0300

    criação da feature para editar contato.

--- app.py
+++ app.py
@@ -2,11 +2,14 @@ def exibe_titulo():
     print("**************************************************")
     print("***** AGENDA TELEFÔNICA")
     print("**************************************************\n")
+    return
 
 def exibe_opcoes_menu():
     print("1. adicionar contato")
     print("2. vizualizar lista de contatos")
+    print("3. editar contato")
     print("6. sair")
+    return
 
 def adicionar_contato(contatos):
     nome = input("digite o nome do novo contato: ")
@@ -15,6 +18,7 @@ def adicionar_contato(contatos):
     contato = {"Nome": nome, "Telefone": telefone, "Email": email ,"Favorito":False}
     contatos.append(contato)
     vizualizar_contatos(contatos)
+    return
 
 def vizualizar_contatos(contatos):
     print("\nminha lista de contatos:")
@@ -24,8 +28,26 @@ def vizualizar_contatos(contatos):
     print()
     return
 
-contatos = [{"Nome":"Rafael", "Telefone":"(11)99299-9483", "Email":"vansan.rafael@gmail.com" ,"Favorito":False},
-            {"Nome":"Pam", "Telefone":"(11)98033-3796", "Email":"vansan.pam@gmail.com" ,"Favorito":False}]
+def editar_contato(contatos):
+    vizualizar_contatos(contatos)
+    while True:
+        try:
+            index_contato_editar = int(input("digite a chave do contato que deseja editar: "))
+            index_ajustada = index_contato_editar - 1
+            contato = contatos[index_ajustada]
+        except:
+            print("chave inválida!")
+        else:
+            contatos[index_ajustada]["Nome"] = input("digite o novo nome do novo: ")
+            contatos[index_ajustada]["Telefone"] = input("digite o novo telefone do novo: ")
+            contatos[index_ajustada]["Email"] = input("digite o novo email do novo: ")
+            vizualizar_contatos(contatos)
+            break
+    return
+
+contatos = [{"Nome":"Contato 1", "Telefone":"(11)99999-9999", "Email":"contato1@email.com" ,"Favorito":False},
+            {"Nome":"Contato 2", "Telefone":"(11)88888-8888", "Email":"contato2@email.com" ,"Favorito":False}]
+
 while True:
     exibe_titulo()
     exibe_opcoes_menu()
@@ -39,7 +61,8 @@ while True:
             adicionar_contato(contatos)
         if opcao == 2:
             vizualizar_contatos(contatos)
-
+        if opcao == 3:
+            editar_contato(contatos)
         elif opcao == 6:
             break
 
