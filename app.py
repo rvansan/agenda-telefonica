dd46fb696e01d15503e0c32b289a3092266f8eaf
Rafael Vansan <rafaelvansan@MacBook-Air-de-Rafael.local>
2025-07-03 13:51:36 -0300

    criação da feature para exibir lista de contatos favoritos.

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ def exibe_opcoes_menu():
     print("1. adicionar contato")
     print("2. vizualizar lista de contatos")
     print("3. editar contato")
+    print("4. marcar/ desmarcar como favorito")
+    print("5. exibir lista de contatos favoritos")
     print("6. sair")
     return
 
@@ -24,7 +26,8 @@ def vizualizar_contatos(contatos):
     print("\nminha lista de contatos:")
     for index, contato in enumerate(contatos):
         index_ajustado = index + 1
-        print(f"{index_ajustado}. {contato["Nome"]} | {contato["Telefone"]} | {contato["Email"]}")
+        favorito = " | Favorito" if contato["Favorito"] else ""
+        print(f"{index_ajustado}. {contato["Nome"]} | {contato["Telefone"]} | {contato["Email"]} {favorito}")
     print()
     return
 
@@ -45,8 +48,35 @@ def editar_contato(contatos):
             break
     return
 
+def marcar_desmarcar_favorito(contatos):
+    vizualizar_contatos(contatos)
+    while True:
+        try:
+            index_contato_editar = int(input("digite a chave do contato que deseja marcar/ desmarcar como favorito: "))
+            index_ajustada = index_contato_editar - 1
+            contato = contatos[index_ajustada]
+        except:
+            print("chave inválida!")
+        else:
+            contatos[index_ajustada]["Favorito"] = not contatos[index_ajustada]["Favorito"]
+            vizualizar_contatos(contatos)
+            break
+    return
+
+def exibir_favoritos(contatos):
+    print("\nminha lista de contatos favoritos:")
+    for index, contato in enumerate(contatos):
+        if contato["Favorito"]:
+            index_ajustado = index + 1
+            favorito = " | Favorito" if contato["Favorito"] else ""
+            print(f"{index_ajustado}. {contato["Nome"]} | {contato["Telefone"]} | {contato["Email"]} {favorito}")
+    print()
+    return
+
+
 contatos = [{"Nome":"Contato 1", "Telefone":"(11)99999-9999", "Email":"contato1@email.com" ,"Favorito":False},
-            {"Nome":"Contato 2", "Telefone":"(11)88888-8888", "Email":"contato2@email.com" ,"Favorito":False}]
+            {"Nome":"Contato 2", "Telefone":"(11)88888-8888", "Email":"contato2@email.com" ,"Favorito":True},
+            {"Nome":"Contato 3", "Telefone":"(11)77777-7777", "Email":"contato3@email.com" ,"Favorito":False}]
 
 while True:
     exibe_titulo()
@@ -59,10 +89,14 @@ while True:
     else:
         if opcao == 1:
             adicionar_contato(contatos)
-        if opcao == 2:
+        elif opcao == 2:
             vizualizar_contatos(contatos)
-        if opcao == 3:
+        elif opcao == 3:
             editar_contato(contatos)
+        elif opcao == 4:
+            marcar_desmarcar_favorito(contatos)
+        elif opcao == 5:
+            exibir_favoritos(contatos)
         elif opcao == 6:
             break
 
