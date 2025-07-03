3a1dec851df813185e92eec1c4da8dba18c21b09
Rafael Vansan <rafaelvansan@MacBook-Air-de-Rafael.local>
2025-07-03 13:28:12 -0300

    criação da fearture de adicionar novo contato.

--- app.py
+++ app.py
@@ -4,8 +4,17 @@ def exibe_titulo():
     print("**************************************************\n")
 
 def exibe_opcoes_menu():
-    print("1. vizualizar lista de contatos")
+    print("1. adicionar contato")
+    print("2. vizualizar lista de contatos")
     print("6. sair")
+
+def adicionar_contato(contatos):
+    nome = input("digite o nome do novo contato: ")
+    telefone = input("digite o telefone do novo contato: ")
+    email = input("digite o email do novo contato: ")
+    contato = {"Nome": nome, "Telefone": telefone, "Email": email ,"Favorito":False}
+    contatos.append(contato)
+    vizualizar_contatos(contatos)
 
 def vizualizar_contatos(contatos):
     print("\nminha lista de contatos:")
@@ -27,7 +36,10 @@ while True:
         print("opção inválida!")
     else:
         if opcao == 1:
+            adicionar_contato(contatos)
+        if opcao == 2:
             vizualizar_contatos(contatos)
+
         elif opcao == 6:
             break
 
